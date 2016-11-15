de99586e843a2ab26d3321ad81f3fb5a28b75245
David Ainslie <dainslie@gmail.com>
2016-11-15 13:52:29 +0000

    Latest rtp-mongo-lib

--- project/Build.scala
+++ project/Build.scala
@@ -34,7 +34,7 @@ object Build extends Build {
     .settings(libraryDependencies ++= {
       val `rtp-io-lib-version` = "1.7.23"
       val `rtp-test-lib-version` = "1.3.4"
-      val `rtp-mongo-lib-version` = "3.0.3"
+      val `rtp-mongo-lib-version` = "3.0.4-SNAPSHOT"
 
       Seq(
         "joda-time" % "joda-time" % "2.5",
